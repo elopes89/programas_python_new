6c7c1675593b3eb1797fe0de31ae3b5ede71a109
Rafael <rafavilvert@hotmai.com>
2021-06-26 19:48:16 -0300

    Primeiro Commit

---
+++ sa2_2.py
@@ -0,0 +1,11 @@
+n1 = int(input('Primeiro número '))
+n2 = int(input('Segundo número '))
+n3 = int(input('Terceiro número '))
+n4 = int(input('Quarto número '))
+
+media = (n1 + n2 + n3 + n4) / 4
+print(media)
+if media > 0:
+    print('positivo')
+else:
+    print('negativo')
