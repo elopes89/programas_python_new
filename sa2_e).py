79f0434738de42c520025bf5f99938387db5dafe
elopes89 <86383785+elopes89@users.noreply.github.com>
2021-06-30 02:27:24 -0300

    Add files via upload

--- sa2_e).py
+++ sa2_e).py
@@ -1,3 +1,10 @@
-l = [5, 7, 2, 9, 4, 1, 3]
-l.sort()
-print(l)
+lista = [5, 7, 2, 9, 4, 1, 3]
+auxiliar = 0
+for i in range(0,len(lista)):
+    for j in range(0,len(lista)-1):
+        if lista[i] < lista[j]:
+            auxiliar = lista[i]
+            lista[i] = lista[j]
+            lista[j] = auxiliar
+for i in lista:
+    print(f'{i} ',end=' ')
